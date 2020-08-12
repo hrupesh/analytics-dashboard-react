0358d5404752acfe89cd63e715609f1e6497af5f
hrupesh <rcashok0@gmail.com>
2020-08-12 17:39:49 +0530

    Auto Commit 😎

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -15,43 +15,29 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 // reactstrap components
 import {
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  Form,
-  FormGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Input,
-  InputGroup,
+  // DropdownMenu,
+  // DropdownItem,
+  // UncontrolledDropdown,
+  // DropdownToggle,
+  // Form,
+  // FormGroup,
+  // InputGroupAddon,
+  // InputGroupText,
+  // Input,
+  // InputGroup,
   Navbar,
   Nav,
   Container,
-  Media,
-  Dropdown,
+  // Media,
+  // Dropdown,
 } from "reactstrap";
+import { Dropdown, DropdownItem, DropdownMenu, Icon } from "@duik/it";
 
 class AdminNavbar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dropdownOpen: false,
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-
-  toggle(event) {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
-    });
-  }
-
   render() {
     return (
       <>
@@ -68,77 +54,12 @@ class AdminNavbar extends React.Component {
             >
               {this.props.brandText}
             </Link>
-            {/* <Form className="navbar-search navbar-search-light form-inline mr-3 d-none d-md-flex ml-lg-auto">
-              <FormGroup className="mb-0">
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="fas fa-search" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input placeholder="Search" type="text" />
-                </InputGroup>
-              </FormGroup>
-            </Form> */}
+
             <Nav className="align-items-center d-none d-md-flex" navbar>
-              {/* <UncontrolledDropdown nav>
-                <DropdownToggle className="pr-0" nav>
-                  <Media className="align-items-center">
-                    <span className="avatar avatar-sm rounded-circle">
-                      <img
-                        alt="..."
-                        src={require("assets/img/theme/team-4-800x800.jpg")}
-                      />
-                    </span>
-                    <Media className="ml-2 d-none d-lg-block">
-                      <span className="mb-0 text-sm font-weight-bold text-dark">
-                        Jessica Jones
-                      </span>
-                    </Media>
-                  </Media>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-arrow" right>
-                  <DropdownItem className="noti-title" header tag="div">
-                    <h6 className="text-overflow m-0">Welcome!</h6>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-single-02" />
-                    <span>My profile</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-settings-gear-65" />
-                    <span>Settings</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-calendar-grid-58" />
-                    <span>Activity</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-support-16" />
-                    <span>Support</span>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem
-                    href="#pablo"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <i className="ni ni-user-run" />
-                    <span>Logout</span>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown> */}
-              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                <DropdownToggle
-                  onChange={(e) => alert(e.target.value)}
-                  defaultValue="ENG"
-                >
-                  Dropdown
-                </DropdownToggle>
-                <DropdownMenu >
-                  <DropdownItem defaultValue="ENG">ENG</DropdownItem>
-                  <DropdownItem defaultValue="HIN">HIN</DropdownItem>
-                  <DropdownItem defaultValue="ESP">ESP</DropdownItem>
-                </DropdownMenu>
+              <Dropdown buttonText={<strong>Click me</strong>}>
+                <DropdownItem>ENG</DropdownItem>
+                <DropdownItem>ESP</DropdownItem>
+                <DropdownItem>HIN</DropdownItem>
               </Dropdown>
             </Nav>
           </Container>
