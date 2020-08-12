713a81cc29cb14ecb4e9fb4de19aaecc53f27351
hrupesh <rcashok0@gmail.com>
2020-08-13 02:01:47 +0530

    Auto Commit 😎

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -24,7 +24,10 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+        <div
+          className="header pb-8 pt-5 pt-md-8"
+          style={{ backgroundColor: "#fbfbfd" }}
+        >
           <Container fluid>
             <div className="header-body">
               {/* Card stats */}
