24a709c812823e2639d517e5901f933a6c398619
hrupesh <rcashok0@gmail.com>
2020-08-14 19:58:19 +0530

    Auto Commit 😎

--- src/variables/charts.js
+++ src/variables/charts.js
@@ -481,18 +481,19 @@ let chartExample2 = {
 };
 
 const data1 = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [
     {
+      label:"",
       fill: true,
-      lineTension: 0,
+      lineTension: 0.4,
       backgroundColor: '#e7effb',
       borderColor: '#1664d7',
       borderCapStyle: 'square',
-      pointBorderWidth: 0.1,
+      pointBorderWidth: 1,
+      borderWidth:2,
       pointRadius: 0,
       pointHitRadius: 0,
-      data: [65, 59, 80, 81, 56, 55, 40]
+      data: [21,98, 12, 82, 12, 95, 30,83,38,92,34,85,29]
     }
   ]
 };
