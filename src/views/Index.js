c21131c1ece2d070222ae6a0bdf178d7e3321a80
hrupesh <rcashok0@gmail.com>
2020-08-15 16:36:51 +0530

    Auto Commit 😎

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 // node.js library that concatenates classes (strings)
 import classnames from "classnames";
@@ -438,7 +421,10 @@ class Index extends React.Component {
                     </div>
                   </Row>
                 </CardHeader>
-                <Table className="align-items-center table-flush cstm-table" responsive>
+                <Table
+                  className="align-items-center table-flush cstm-table"
+                  responsive
+                >
                   <thead className="thead-light">
                     <tr>
                       <th scope="col">Network</th>
@@ -447,7 +433,7 @@ class Index extends React.Component {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr >
+                    <tr>
                       <th scope="row">Instagram</th>
                       <td>3,550</td>
                       <td>
