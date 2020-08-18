59fe2e8163e7a1e8af2a1795b563629d9c7b8034
hrupesh <rcashok0@gmail.com>
2020-08-18 23:50:14 +0530

    Auto Commit 😎

--- src/views/Index.js
+++ src/views/Index.js
@@ -112,20 +112,6 @@ class Index extends React.Component {
                         ]}
                         defaultOption={{ label: "January", value: 1 }}
                       />
-                      <select defaultValue="1" className="custom-select">
-                        <option value="1">January</option>
-                        <option value="2">February</option>
-                        <option value="3">March</option>
-                        <option value="4">April</option>
-                        <option value="5">May</option>
-                        <option value="6">June</option>
-                        <option value="7">July</option>
-                        <option value="8">August</option>
-                        <option value="9">September</option>
-                        <option value="10">October</option>
-                        <option value="11">November</option>
-                        <option value="12">December</option>
-                      </select>
                     </div>
                   </Row>
                 </CardHeader>
